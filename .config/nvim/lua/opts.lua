51d4b07d0d63451f414432e6cd653b2fa036ed1e
Skyler Richter <skylerrichter@gmail.com>
2024-02-05 09:03:47 -0800

    Disable mouse

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -1,18 +1,27 @@
+-- Show line numbers.
 vim.opt.nu = true
 
+-- Configure tabs.
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Disable swap and backup files.
 vim.opt.swapfile = false
 vim.opt.backup = false
 
+-- Disable highlighting search.
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Enable terminal colors.
 vim.opt.termguicolors = true
 
+-- Disable mouse support.
+vim.opt.mouse = ""
+
+-- Set leader key.
 vim.g.mapleader = " "
 
