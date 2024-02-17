440a5804648c6608fb1d65b9a99f3e72b7962b89
Skyler Richter <skylerrichter@gmail.com>
2024-02-17 14:05:04 -0800

    Remove synthwave theme

--- .config/nvim/lua/plugins/synthwave84.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'artanikin/vim-synthwave84',
-  config = function()
-    -- vim.cmd('colorscheme synthwave84')
-    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-  end
-}
-
