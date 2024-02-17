872b18085df5b60f01a10d5a80c6f432cddf2661
Skyler Richter <skylerrichter@gmail.com>
2024-02-17 15:52:13 -0800

    Sync colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,25 @@
 local wezterm = require 'wezterm'
 
+function get_appearance()
+    if wezterm.gui then
+        return wezterm.gui.get_appearance()
+    end
+    return 'Dark'
+    end
+
+    function scheme_for_appearance(appearance)
+    if appearance:find 'Dark' then
+        return 'rose-pine-moon'
+    else
+        return 'rose-pine-dawn'
+    end
+end
+
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.color_scheme = 'rose-pine-moon'
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 config.line_height = 1.2
 
