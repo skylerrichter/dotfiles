b042dc82c8a1724a6cfe5b6654b8f58ac79d1dba
Skyler Richter <skylerrichter@gmail.com>
2024-02-17 16:01:48 -0800

    Sync with vscode

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,9 +19,13 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
+config.font = wezterm.font 'Cascadia Code'
+
 config.color_scheme = scheme_for_appearance(get_appearance())
 
-config.line_height = 1.2
+config.font_size = 12.0
+
+config.line_height = 1.4
 
 return config
 
