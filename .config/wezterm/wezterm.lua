5f585816442a8914e8a9b1217596bd27871306ee
Skyler Richter <skylerrichter@gmail.com>
2024-02-19 20:48:41 -0800

    Configure zellij

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,27 +1,12 @@
 local wezterm = require 'wezterm'
 
-function get_appearance()
-    if wezterm.gui then
-        return wezterm.gui.get_appearance()
-    end
-    return 'Dark'
-    end
-
-    function scheme_for_appearance(appearance)
-    if appearance:find 'Dark' then
-        return 'rose-pine-moon'
-    else
-        return 'rose-pine-dawn'
-    end
-end
-
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font 'Cascadia Code'
 
-config.color_scheme = scheme_for_appearance(get_appearance())
+config.color_scheme = 'rose-pine-moon'
 
 config.font_size = 12.0
 
